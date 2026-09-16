ab212db6a94031caa0ca611c4704608ef42c80e4
Troy Johnson <troy.johnson57@gmail.com>
2026-09-15 23:01:38 -0600

    feat: show team logos on the pick

--- components/TeamLogo.tsx
+++ components/TeamLogo.tsx
@@ -2,7 +2,7 @@ import styles from "./TeamLogo.module.css";
 
 const espnTeamIds: Record<string, string> = {LA: "lar", WAS: "wsh"};
 
-export function TeamLogo({team}: {team: string}) {
+export function TeamLogo({team, size = 28}: {team: string; size?: number}) {
   const teamId = espnTeamIds[team] ?? team.toLowerCase();
-  return <img alt={`${team} logo`} className={styles.logo} height={28} src={`https://a.espncdn.com/i/teamlogos/nfl/500/${teamId}.png`} width={28}/>;
+  return <img alt={`${team} logo`} className={styles.logo} height={size} src={`https://a.espncdn.com/i/teamlogos/nfl/500/${teamId}.png`} width={size}/>;
 }
